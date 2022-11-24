f03eba2b0cba84f9e01c21c72767b3e6bcffd966
dev-splin <dev.splin@gmail.com>
2022-11-24 11:44:17 +0900

    ADD : [React] crud - control 컴포넌트 추가

---
+++ src/components/Control.js
@@ -0,0 +1,32 @@
+import {Component} from "react";
+
+class Control extends Component {
+    render() {
+        return (
+            <ul>
+                <li><a
+                    href="/create"
+                    onClick={function (e) {
+                        e.preventDefault();
+                        this.props.onChangeMode('create');
+                    }.bind(this)}
+                >create</a></li>
+                <li><a
+                    href="/update"
+                    onClick={function (e) {
+                        e.preventDefault();
+                        this.props.onChangeMode('update');
+                    }.bind(this)}
+                >update</a></li>
+                <li><button
+                    type="button"
+                    onClick={function (e) {
+                        this.props.onChangeMode('delete');
+                    }.bind(this)}
+                >delete</button></li>
+            </ul>
+        );
+    }
+}
+
+export default Control;
